2878f4ae1967bc9fa6f767233bd6d22fb64e3703
Hugo Targino <hugo_targino@outlook.com>
2016-01-03 16:08:20 -0200

    Adicionando ClientController

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace AppLaravel\Http\Controllers;
+
+use AppLaravel\Client;
+use Illuminate\Http\Request;
+
+use AppLaravel\Http\Requests;
+use AppLaravel\Http\Controllers\Controller;
+
+class ClientController extends Controller
+{
+
+    private $client;
+
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function index()
+    {
+        return $this->client->all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
