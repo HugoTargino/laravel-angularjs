69f67a085545deb57ab0abbd7c808204958fd137
Hugo Targino <hugo_targino@outlook.com>
2016-02-01 15:08:39 -0200

    Criando ProjectMember e finalizando permissions

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -70,7 +70,7 @@ class ProjectController extends Controller
      */
     public function show($id)
     {
-        if($this->checkProjectOwner($id) == false){
+        if($this->checkProjectPermissions($id) == false){
             return ['error' => 'Access Forbidden'];
         }
 
@@ -128,5 +128,24 @@ class ProjectController extends Controller
         return $this->repository->isOwner($projectId, $userId);
 
 
+    }
+
+    private function checkProjectMember($projectId)
+    {
+
+        $userId = Authorizer::getResourceOwnerId();
+
+        return $this->repository->hasMember($projectId, $userId);
+
+
+    }
+
+    private function checkProjectPermissions($projectId)
+    {
+        if($this->checkProjectOwner($projectId) or $this->checkProjectMember($projectId)){
+            return true;
+        }
+
+        return false;
     }
 }
